4f3ee8715ba3ff151bc60b97d344cf8aa5c18ef8
Nick Schot <nickschot@gmail.com>
2019-04-02 16:18:40 +0200

    move blog to subroute add old site content in index.hbs

---
+++ app/initializers/routes.js
@@ -0,0 +1,15 @@
+import Router from '../router';
+
+export function initialize() {
+  Router.map(function() {
+    this.route('blog');
+    this.route('post', { path: '/blog/:id' });
+    this.route('page', { path: '/blog/page/:id' });
+    this.route('author', { path: '/blog/author/:id'});
+    this.route('tag', { path: '/blog/tag/:id' });
+  });
+}
+
+export default {
+  initialize
+};
---
+++ app/routes/index.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  classNames: ['index-template', 'home-template'],
+
+  model(){},
+});
