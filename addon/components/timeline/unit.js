0f4bf1509222342b9690e08af0a40f95c2b5cd68
Nick Schot <nickschot@gmail.com>
2019-04-04 17:50:13 +0200

    work on theme

--- addon/components/timeline/unit.js
+++ addon/components/timeline/unit.js
@@ -1,6 +1,15 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/timeline/unit';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  layout
+  layout,
+
+  computedClasses: computed('type', function(){
+    if(this.type === 'right'){
+      return `pl-5 ml-auto`;
+    } else {
+      return `pr-5`;
+    }
+  })
 });
