310b6c6567ca1e09a880206c13091e0007b61560
Nick Schot <nickschot@gmail.com>
2019-04-06 12:46:32 +0200

    work work work

---
+++ addon/components/transition-in.js
@@ -0,0 +1,21 @@
+import Component from '@ember/component';
+import InViewportMixin from 'ember-in-viewport';
+import { computed } from '@ember/object';
+
+export default Component.extend(InViewportMixin, {
+  classNames: ['slide-up'],
+  classNameBindings: ['type', 'isVisibleClass'],
+
+  type: 'slide-up',
+  intersectionThreshold: 1,
+
+  didEnterViewport() {
+    this.set('isVisible', true);
+  },
+
+  isVisibleClass: computed('isVisible', 'type', function(){
+    if(this.isVisible){
+      return `${this.type}--active`;
+    }
+  })
+});
