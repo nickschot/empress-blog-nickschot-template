8fef50992b4a53d27492f1e35a329d25536d1e30
Nick Schot <nickschot@gmail.com>
2019-04-06 19:35:15 +0200

    work work work

--- addon/components/timeline.js
+++ addon/components/timeline.js
@@ -1,10 +1,18 @@
 import Component from '@ember/component';
 import layout from '../templates/components/timeline';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
 
   currentType: false,
+
+  studyClass: computed('currentType', function(){
+    return this.currentType === 'study' ? 'timeline-btn--active' : '';
+  }),
+  workClass: computed('currentType', function(){
+    return this.currentType === 'work' ? 'timeline-btn--active' : '';
+  }),
 
   actions: {
     updateItems(type){
